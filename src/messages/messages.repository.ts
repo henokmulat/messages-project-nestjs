ec6fc09b5ef79d977c16e82193ccd1e071c5d457
henokmulat <henokmulat27@gmail.com>
2025-10-25 16:22:02 +0300

    Create a repository class

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,11 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessageRepository {
+  async findOne(id: number) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+  async findAll() {}
+  async create(message: string) {}
+}
